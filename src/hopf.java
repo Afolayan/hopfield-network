019b25f22c09d61f43a2c629b60ca691c4f32a00
danielgynn <danielgynn@hotmail.co.uk>
2017-04-02 18:04:57 +0100

    Move code to single file and comment

--- src/hopf.java
+++ src/hopf.java
@@ -1,18 +1,56 @@
-package hopf;
-
 import java.util.*;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+/**
+ * Class Main (hopf)
+ * @author Daniel Gynn (DJG44)
+ * Stores the input values from the console and runs the Hopfield network with the specified patterns.
+ */
+public class hopf {
+    static Hopfield trainingPattern;
+
+	public static void main(String[] args) throws IOException {
+		// Get pattern file paths from command line input
+		String storedPatternInput = args[0];
+		String incompletePatternInput = args[1];
+
+		// Store patterns, by line, from file reader
+		String[] sp = Reader.readFile(storedPatternInput);
+		String[] ip = Reader.readFile(incompletePatternInput);
+
+		for (int trainingPatterns = 0; trainingPatterns < sp.length; trainingPatterns++) {
+			float[] storedPattern = Hopfield.getPattern(sp[trainingPatterns]);
+
+			// Get training pattern and run the learning function.
+			trainingPattern = new Hopfield(storedPattern.length);
+			trainingPattern.learn(storedPattern);
+		}
+
+		for (int i = 0; i < ip.length; i++) {
+		    // Get the patterns into int array format
+			float[] incompletePattern = Hopfield.getPattern(ip[i]);
+
+			// Generate the network output.
+	        Hopfield.generateOutput(trainingPattern, incompletePattern);
+		}
+	}
+}
 
 /**
  * Class Hopfield
  * @author Daniel Gynn (DJG44)
- *
+ * Runs the Hopfield neural network.
+ * Some inspiration for this Class taken from the book 'Practical Artificial Intelligence
+ * Programming With Java' by Mark Watson (2008). 
  */
-public class Hopfield {
+class Hopfield {
     static int patternLength;
     float[][] weights;
     float[] tempStorage;
     Vector<float[]> trainingPattern = new Vector<float[]>();
-    
+
     /**
      * Hopfield class constructor
      * @param patternLength
@@ -22,37 +60,31 @@ public class Hopfield {
         weights = new float[patternLength][patternLength];
         tempStorage = new float[patternLength];
     }
-    
-//	public static float[] getTrainingData(String[] sp) {
-//		// For each line in the training 
-//		for (int i = 0; i < sp.length;) {
-//		    // Get the stored patterns into int array format, by lines.
-//		    float[] storedPattern = Hopfield.getPattern(sp[i]);
-//		    System.out.println("GETDATA: " + storedPattern);
-//		    return storedPattern;
-//		}
-//		return null;
-//	}
-    
-    static void generateOutput(Hopfield test, float[] pattern) {
+
+    /**
+     * Generates the appropriate output for the given patterns.
+     * @param Hopfield run
+     * @param float[] pattern
+     */
+    static void generateOutput(Hopfield run, float[] pattern) {
         float[] data = new float[patternLength];
-        
+
         for (int i = 0; i < patternLength; i++) {
           data[i] = pattern[i];
         }
-        
+
         for (int i = 0; i < 1; i++) {
           int index = (int) ((patternLength - 1) * (float) Math.random());
           data[index] = -data[index];
-          
+
           if (data[index] < 0.0f) {
               data[index] = 1.0f;
           } else {
               data[index] = -1.0f;
           }
         }
- 
-        float[] node = test.makeNetwork(data, patternLength);
+
+        float[] node = run.makeNetwork(data, patternLength);
         int output = 0;
 
         for (int i = 0; i < patternLength; i++) {
@@ -61,12 +93,17 @@ public class Hopfield {
         	} else {
         		output = -1;
         	}
-        	
+
         	System.out.print(output + " ");
         }
         System.out.println();
     }
-    
+
+    /**
+     * Takes the input text from the file as a String and returns as an array of Floats
+     * @param String patternString
+     * @return float[] patten
+     */
     public static float[] getPattern(String patternString) {
 		String[] patternNodes = patternString.split(" ");
 		float[] pattern = new float[patternNodes.length];
@@ -82,36 +119,46 @@ public class Hopfield {
 		return pattern;
 	}
 
+    /**
+     * Takes the stored pattern and runs the training algorithm.
+     * @param float[] pattern
+     */
     public void learn(float[] pattern) {
     	trainingPattern.addElement(pattern);
-        
+
         for (int i = 1; i < patternLength; i++) {
             for (int j = 0; j < i; j++) {
                 for (int n = 0; n < trainingPattern.size(); n++) {
                     float[] data = (float[]) trainingPattern.elementAt(n);
                     float temp = data[i] * data[j] + weights[j][i];
-                    
+
                     weights[i][j] = weights[j][i] = temp;
                 }
             }
         }
-        
+
         for (int i = 0; i < patternLength; i++) {
             tempStorage[i] = 0.0f;
-            
+
             for (int j = 0; j < i; j++) {
                 tempStorage[i] += weights[i][j];
             }
         }
     }
 
+    /**
+     * Builds the Hopfield network for the given pattern and returns an array of nodes.
+     * @param float[] pattern
+     * @param int numIterations
+     * @return nodes
+     */
     public float[] makeNetwork(float[] pattern, int numIterations) {
     	float[] nodes = new float[patternLength];
-    	
+
         for (int i = 0; i < patternLength; i++) {
         	nodes[i] = pattern[i];
         }
-   
+
         for (int i = 0; i < numIterations; i++) {
             for (int j = 0; j < patternLength; j++) {
                 if (energy(j, nodes) > 0.0f) {
@@ -121,17 +168,56 @@ public class Hopfield {
                 }
             }
         }
-        
+
         return nodes;
     }
 
+    /**
+     * Delta energy function.
+     * @param int index
+     * @param float[] inputNodes
+     * @return
+     */
     private float energy(int index, float[] inputNodes) {
         float temp = 0.0f;
-        
+
         for (int i = 0; i < patternLength; i++) {
             temp += weights[index][i] * inputNodes[i];
         }
-        
+
         return 2.0f * temp - tempStorage[index];
     }
+}
+
+/**
+ * Class Reader
+ * @author Daniel Gynn (DJG44)
+ * Reads in the target file and returns the text output as a String.
+ */
+class Reader {
+	public static String[] readFile(String strings) throws IOException {
+		BufferedReader BufferedReader = new BufferedReader(new FileReader(strings));
+		String str;
+
+		try {
+			ArrayList<String> list = new ArrayList<String>();
+			while((str = BufferedReader.readLine()) != null){
+			    list.add(str);
+			}
+
+			String[] stringArr = list.toArray(new String[0]);
+			return stringArr;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (BufferedReader != null) {
+					BufferedReader.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return null;
+	}
 }
